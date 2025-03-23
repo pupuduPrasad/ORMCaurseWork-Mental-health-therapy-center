99f949fd2488801ebb6fd4c32bccc14e8c607d0d
pupuduPrasad <prasadjayasinghe142@gmail.com>
2025-03-23 19:14:58 +0530

    create the FactoryConfiguration ,dependancy add and hibernate add

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module lk.ijse.gdse.ormcaursework.ormcaursework {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.hibernate.orm.core;
 
 
     opens lk.ijse.gdse.ormcaursework to javafx.fxml;
