689da3fe12f21ede80add64bcd59a6ab9de3f0ae
pupuduPrasad <prasadjayasinghe142@gmail.com>
2025-04-21 21:19:41 +0530

    updated

--- src/main/java/lk/ijse/gdse/ormcaursework/HelloApplication.java
+++ src/main/java/lk/ijse/gdse/ormcaursework/HelloApplication.java
@@ -11,9 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/view/HomePage.fxml"));
+        stage.setTitle("MENTAL HEALTH THERAPY CENTER");
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(getClass().getResource("/style/HomeStyle.css").toExternalForm());
-        stage.setTitle("MENTAL HEALTH THERAPY CENTER");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
