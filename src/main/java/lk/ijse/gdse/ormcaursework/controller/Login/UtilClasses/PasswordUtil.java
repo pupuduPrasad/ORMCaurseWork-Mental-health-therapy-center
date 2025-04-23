95d1fda88f4e2bfedd840098d7c80cf2d62cffd2
pupuduPrasad <prasadjayasinghe142@gmail.com>
2025-04-23 08:47:32 +0530

    updated

--- src/main/java/lk/ijse/gdse/ormcaursework/controller/Login/UtilClasses/PasswordUtil.java
+++ src/main/java/lk/ijse/gdse/ormcaursework/controller/Login/UtilClasses/PasswordUtil.java
@@ -3,15 +3,14 @@ package lk.ijse.gdse.ormcaursework.controller.Login.UtilClasses;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordUtil {
-
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    // Hash (encrypt) password
+//    (encrypt) password
     public static String hashPassword(String plainPassword) {
         return encoder.encode(plainPassword);
     }
 
-    // Match raw password with hashed password
+//    Match raw password with hashed password
     public static boolean matches(String rawPassword, String hashedPassword) {
         return encoder.matches(rawPassword, hashedPassword);
     }
